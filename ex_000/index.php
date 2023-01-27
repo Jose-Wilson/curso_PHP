d8b553dbe6f1eeaa9609ee00a8c07085daf5d90b
José Wilson <92189182+Jose-Wilson@users.noreply.github.com>
2023-01-27 13:35:15 -0200

    Initial commit

---
+++ ex_000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Deus é o meu Senhor. \u{1F30E}"; 
+        ?>
+    </h1>
+    <p>E! Jesus o Cristo, é o seu filho que ele nos enviou para nos Salvar.</p>
+</body>
+</html>
